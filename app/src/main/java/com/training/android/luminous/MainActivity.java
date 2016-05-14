e2ac2d6ac937c4f732921698b0c32e33dddea17f
Ahdzlee Formentera <ahdzlee@Ahdzlees-MacBook-Pro.local>
2016-05-14 09:38:56 +0800

    Implemented switching the light bulb on and off.

--- app/src/main/java/com/training/android/luminous/MainActivity.java
+++ app/src/main/java/com/training/android/luminous/MainActivity.java
@@ -1,13 +1,33 @@
 package com.training.android.luminous;
 
-import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.widget.CompoundButton;
+import android.widget.ImageView;
+import android.widget.ToggleButton;
 
-public class MainActivity extends AppCompatActivity {
+public class MainActivity extends AppCompatActivity implements CompoundButton.OnCheckedChangeListener {
+
+    private ImageView    mImgLights;
+    private ToggleButton mBtnSwitch;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        mImgLights = (ImageView) findViewById(R.id.imgLights);
+        mBtnSwitch = (ToggleButton) findViewById(R.id.btnSwitch);
+
+        mBtnSwitch.setOnCheckedChangeListener(this);
+    }
+
+    @Override
+    public void onCheckedChanged(CompoundButton buttonView, boolean isChecked) {
+        if (isChecked) {
+            mImgLights.setImageResource(R.drawable.lights_on);
+        } else {
+            mImgLights.setImageResource(R.drawable.lights_off);
+        }
     }
 }
